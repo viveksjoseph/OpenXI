c1da693ae13432530497308628b27937f1f3f5ab
Vivek SJ <vivek@Viveks-iMac.local>
2015-09-27 21:38:28 +0530

    committing make files for classes and edit in .classpath

--- OpenXI/proj.android/jni/Android.mk
+++ OpenXI/proj.android/jni/Android.mk
@@ -12,9 +12,23 @@ LOCAL_MODULE_FILENAME := libcocos2dcpp
 
 LOCAL_SRC_FILES := hellocpp/main.cpp \
                    ../../Classes/AppDelegate.cpp \
-                   ../../Classes/HelloWorldScene.cpp
+                   ../../Classes/HelloWorldScene.cpp \
+                   ../../Classes/CharacterMechanics/GenericAICharacter.cpp \
+                   ../../Classes/CharacterMechanics/GenericPlayerCharacter.cpp \
+                   ../../Classes/CharacterMechanics/InventoryItem.cpp \
+                   ../../Classes/ExtraUtils/InfiniteParallaxNode.cpp \
+                   ../../Classes/GameMechanics/GameMasterControl.cpp \
+                   ../../Classes/GameScreens/GenericSpeechBar.cpp \
+                   ../../Classes/GameScreens/InGameHUD.cpp \
+                   ../../Classes/GameScreens/InGameSettings.cpp \
+                   ../../Classes/GameScreens/MainScreen.cpp \
+                   ../../Classes/GameScreens/PlayerInventoryHud.cpp \
 
-LOCAL_C_INCLUDES := $(LOCAL_PATH)/../../Classes
+LOCAL_C_INCLUDES := $(LOCAL_PATH)/../../Classes \
+					$(LOCAL_PATH)/../../Classes/CharacterMechanics \
+					$(LOCAL_PATH)/../../Classes/ExtraUtils \
+					$(LOCAL_PATH)/../../Classes/GameMechanics \
+					$(LOCAL_PATH)/../../Classes/GameScreens \
 
 # _COCOS_HEADER_ANDROID_BEGIN
 # _COCOS_HEADER_ANDROID_END
